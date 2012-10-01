80442ef5584f2d34231da0f6d8f9ab253fec99d9
Jeff Tougas <tougasj@gmail.com>
2012-09-30 22:47:43 -0400

    upping to phantomjs version 1.7.0

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 maintainer        "Gerhard Lazu"
 maintainer_email  "gerhard@lazu.co.uk"
 license           "Apache 2.0"
-description       "Installs phantomjs 1.6.1 from binary build"
+description       "Installs phantomjs 1.7.0 from binary build"
 version           "0.1.0"
 
 recipe "phantomjs", "Installs phantomjs"
